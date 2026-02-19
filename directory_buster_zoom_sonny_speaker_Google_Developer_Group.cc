652b890c7e24315d93173781b5b3e75545169683
HaGa IT Solution <nengkya@yahoo.com>
2026-02-19 21:55:39 +0700

    Add files via upload

--- directory_buster_zoom_sonny_speaker_Google_Developer_Group.cc
+++ directory_buster_zoom_sonny_speaker_Google_Developer_Group.cc
@@ -1,7 +1,8 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include "netdb.h"
-#include <string.h>
+#include <stdio.h> /*fprintf*/
+#include "netdb.h" /*getaddrinfo*/
+#include <string.h> /*memset*/
+
 
 
 int connect_to_host() {
@@ -14,22 +15,55 @@ int connect_to_host() {
 
     struct_addrinfo_hints_pointer = (struct addrinfo *)malloc(sizeof * struct_addrinfo_hints_pointer);
 
+    /*without memset, result in bad value for ai_flags*/
     memset(struct_addrinfo_hints_pointer, 0, sizeof * struct_addrinfo_hints_pointer);
 
+    /*
+    node = host = domain name dari web server
+    restrict = only 1 pointer pointing into memory address, for safety data race condition
+    service = port -> 80
+    hints = give clue to getaddrinfo about internet protocol or socket type that will be used
+    int getaddrinfo(const char * restrict node,
+                    const char * restrict service,
+                    const struct addrinfo * restrict hints,
+                    struct addrinfo ** restrict getaddrinfo_result);
+    */
     int_getaddrinfo_status_code = getaddrinfo("esdm.go.id", "80", struct_addrinfo_hints_pointer, &getaddrinfo_result_pointer);
 
+    /*file print format, getaddrinfo string error*/
     fprintf(stderr, "getaddrinfo from esdm.go.id status code %s\n", gai_strerror(int_getaddrinfo_status_code));
 
     for (struct_addrinfo_pointer = getaddrinfo_result_pointer; struct_addrinfo_pointer != (struct addrinfo *)0; (* struct_addrinfo_pointer).ai_next) {
 
+        /*
+        create or open new socket for connection into web server
+        int socket(int domain, int type, int protocol);
+        */
         int_socket_file_descriptor = socket((* struct_addrinfo_pointer).ai_family,
                                             (* struct_addrinfo_pointer).ai_socktype,
                                             (* struct_addrinfo_pointer).ai_protocol);
 
+        /*
+        means this address cannot even create a socket — try the next one."
+
+        without continue, you would :
+
+        attempt connect() on an invalid descriptor
+
+        trigger undefined behavior
+
+        possibly crash
+        */
         if (int_socket_file_descriptor == -1) continue;
 
+        /*
+        connect the opened port into web server
+        socklen_t is an unsigned int
+        connect(int socket_file_descriptor, const struct sockaddr * socket_address_pointer, socklen_t address length);
+        */
         if (connect(int_socket_file_descriptor, (* struct_addrinfo_pointer).ai_addr, (* struct_addrinfo_pointer).ai_addrlen) == 0) break;
 
+        /*if you do not call close(int_sockect_file_descriptor);, those kernel resources remain allocated*/
         close(int_socket_file_descriptor);
 
     }
@@ -44,26 +78,14 @@ int connect_to_host() {
 
 int check_path() {
 
-    char * request = (char *)malloc(256);
-
     int socket_file_descriptor = connect_to_host();
-
-    /*
-    snprintf -> string n printf
-    int snprintf(char * string, size_t size, const char * format, ...);
-    */
-    snprintf();
-
-
 
     /*
     send data over network with previous connect(); established setting, alias kirim send request to the web server with previous connect(); setting
 
-    buffer is a pointer to the buffer (memory location) containing the data you want to send, in this case is string host server name (domain name)
-
-    size_t send(int socket, const void * buffer, size_t length, int flags);
+    size_t send(int socket, );
     */
-    send(int_socket_file_descriptor, );
+    send();
 
 
 
---
+++ weak_pointer.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include "stdlib.h"
+
+
+struct object {
+
+    struct object_control * pointer_to_struct_object_control;
+
+    int int_value;
+
+};
+
+
+struct object_control {
+
+    int int_strong;
+
+    int int_weak;
+
+    struct object * pointer_to_struct_object; 
+
+};
+
+
+struct object * new_object(int int_value) {
+
+    struct object_control * pointer_to_struct_object_control = (struct object_control *)malloc(sizeof * pointer_to_struct_object_control);
+
+    struct object * pointer_to_struct_object                 = (struct object         *)malloc(sizeof * pointer_to_struct_object);
+
+    (* pointer_to_struct_object_control).int_strong = 1;
+
+    (* pointer_to_struct_object_control).int_weak   = 0;
+
+    (* pointer_to_struct_object_control).pointer_to_struct_object = pointer_to_struct_object;
+
+    (* pointer_to_struct_object).pointer_to_struct_object_control = pointer_to_struct_object_control;
+
+    printf("create object at address %p\n", (void *)pointer_to_struct_object);
+
+    return pointer_to_struct_object;
+
+}
+
+
+struct new_weak_pointer {
+
+    struct object_control * pointer_to_object_control;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+    struct object * struct_object = new_object(12);
+
+    struct weak_pointer struct_weak_pointer = new_weak_pointer(struct_object);
+
+
+
+
+
+
+
+
+    return 0;
+
+}
